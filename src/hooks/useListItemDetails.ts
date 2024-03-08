39e17681a9052312edac45e62b32688086c13ade
Larkuo <larkuowt@gmail.com>
2024-03-08 10:00:10 +0000

    updated useListItemDetails hook with addtional details for details modal tabs

--- src/hooks/useListItemDetails.ts
+++ src/hooks/useListItemDetails.ts
@@ -15,6 +15,23 @@ export interface PokemonDetailsInterface{
     sprite: string;
     types: string[];
     cry: string;
+    about: {
+        height: number;
+        weight: number;
+        abilities: string[];
+    }
+    stats: {
+        hp: number;
+        attack: number;
+        defense: number;
+        special_attack: number;
+        special_defense: number;
+        speed: number;
+    }
+    similar: {
+        name: string;
+        url: string;
+    }[];
 }
 
 export function useListItemDetails(
@@ -23,6 +40,51 @@ export function useListItemDetails(
     pokemonDetails: PokemonDetailsInterface;
 }{
     const [pokemonDetails, setPokemonDetails] = useState<PokemonDetailsInterface>({} as PokemonDetailsInterface);
+
+    async function getSimilarPokemon(pokemonName: string, pokemonTypes: string[]){
+        let similarPokemon: {name: string, url: string}[] = [];
+
+        if(pokemonTypes.length === 1){
+            const pokeApiResponse = await fetch(`https://pokeapi.co/api/v2/type/${pokemonTypes[0]}/`);
+            const pokeApiResponseBody = await pokeApiResponse.json();
+
+            // console.log(`=========================${pokemonName}=======================`);
+
+            const pokemonList = pokeApiResponseBody.pokemon.filter((currentPokemon: any) => 
+                currentPokemon.pokemon.name !== pokemonName
+            ).slice(0,2);
+            // console.log({pokemonList});
+            similarPokemon = pokemonList.map((currentPokemon: any) => currentPokemon.pokemon);
+        }else{
+            const [pokeTypeResponse1, pokeTypeResponse2] = await Promise.all([
+                fetch(`https://pokeapi.co/api/v2/type/${pokemonTypes[0]}/`),
+                fetch(`https://pokeapi.co/api/v2/type/${pokemonTypes[1]}/`)
+            ]);
+            const [pokeTypeResponseBody1, pokeTypeResponseBody2] = await Promise.all([
+                pokeTypeResponse1.json(), pokeTypeResponse2.json()
+            ]);
+
+            // console.log(`=========================${pokemonName}=======================`);
+
+            const pokemonList1 = pokeTypeResponseBody1.pokemon;
+            const pokemonList2 = pokeTypeResponseBody2.pokemon;
+            // console.log({pokemonList1, pokemonList2});
+
+            const commonPokemon = pokemonList1.filter((pokemon1: any) => 
+                pokemonList2.some((pokemon2: any) => pokemon1.pokemon.name === pokemon2.pokemon.name
+            ));
+            // console.log({commonPokemon});
+
+            const filteredList = commonPokemon.filter((currentPokemon: any) => 
+                currentPokemon.pokemon.name !== pokemonName
+            ).slice(0,2);
+            // console.log({filteredList});
+
+            similarPokemon = filteredList;
+        }
+
+        return similarPokemon;
+    }
 
     async function getPokemonDetails(){
         let tempDetails: PokemonDetailsInterface = {} as PokemonDetailsInterface;
@@ -36,6 +98,16 @@ export function useListItemDetails(
                 POKEMON_TYPE_STRINGS[pokeType.type.name]
             );
 
+            let tempStats: any = {};
+            pokeApiResponseBody.stats.forEach((pokeStat: any) => tempStats[String(pokeStat.stat.name).replace('-','_')] = pokeStat.base_stat);
+
+            const similarPokemon = await getSimilarPokemon(pokeApiResponseBody.name,
+                pokeApiResponseBody.types.map((pokeType: PokemonTypeInterface) => 
+                    pokeType.type.name
+                ));
+
+            // console.log({similarPokemon});
+
             tempDetails = {
                 id: pokeApiResponseBody.id,
                 name: pokeApiResponseBody.name,
@@ -43,8 +115,17 @@ export function useListItemDetails(
                     || pokeApiResponseBody.sprites.other['official-artwork'].front_default,
                 cry: pokeApiResponseBody.cries.latest || '',
                 types: pokemonTypes,
+                about: {
+                    height: pokeApiResponseBody.height,
+                    weight: pokeApiResponseBody.weight,
+                    abilities: pokeApiResponseBody.abilities.map((ability: any) => ability.name)
+                },
+                stats: tempStats,
+                similar: similarPokemon,
             };
+
             // console.log({tempDetails, pokeApiResponseBody});
+            // console.log('======================================');
         }
 
         setPokemonDetails(tempDetails);
